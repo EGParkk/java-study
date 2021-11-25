7338101e1cdd6871c31c09ae39a8680d9bdb1190
EGParkk <cooldjsgy@naver.com>
2021-11-25 16:36:27 +0900

    11.15.pm4

--- Exem/src/method/Walking.java
+++ Exem/src/method/Walking.java
@@ -3,8 +3,13 @@ package method;
 public class Walking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double result = calculateCalory(5000);
+		System.out.printf("소모 칼로리 : %.1fkcal", result);
 
+	}
+	
+	private static double calculateCalory(int walk) {
+		return 0.02 * walk;
 	}
 
 }
